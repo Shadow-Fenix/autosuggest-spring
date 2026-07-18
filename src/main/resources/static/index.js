d31429603b419a3fefb12e983b608a3e258c3afe
Nilesh <nilesh2126vp@gmail.com>
2026-07-18 13:04:09 +0530

    java script file updated

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -7,13 +7,18 @@ var user = [
     },
     {
         "name" : "John Doe",
-        "gender" : "Mail",
+        "gender" : "Male",
         "img" : "john.png"
-    }
+    },
+    {
+    "name" : "Asta",
+    "gender" : "Male",
+    "img" : "https://tse4.mm.bing.net/th/id/OIP.L4j_8dGO1ynj0FSWUgUm1gHaGL?r=0&pid=Api&h=220&P=0"
+}
 ];
 var id=0;
 function Toggleuser(){
-    id=(id+1)%2;
+    id=(id+1)%3;
     var userImage = document.getElementById("img");
     userImage.src = user[id].img;
 
@@ -23,4 +28,28 @@ function Toggleuser(){
      var usergender = document.getElementById("gender");
      usergender.innerHTML = user[id].gender;
     
+};
+function randomUser(){
+    fetch('https://randomuser.me/api')
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function(data){
+        var userData = data.results[0];
+
+        var userImage = document.getElementsByName("img");
+        userImage.src = userData.picture.large;
+
+        var userName = document.getElementById("name");
+        userName = userData.name.first + " " + userData.name.last;
+
+        var userGender = document.getElementById("gender");
+        userGender.innerHTML = userData.gender;
+
+
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
